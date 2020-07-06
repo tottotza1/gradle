008c9f33f052a006e9c106a67822217ef1bd71c9
Benjamin Muskalla <bmuskalla@gradle.com>
2020-07-06 15:45:38 +0200

    Remove unnecessary @Unroll annotations from "fileCollections"

--- subprojects/file-collections/src/integTest/groovy/org/gradle/api/file/FileCollectionIntegrationTest.groovy
+++ subprojects/file-collections/src/integTest/groovy/org/gradle/api/file/FileCollectionIntegrationTest.groovy
@@ -21,10 +21,7 @@ import org.gradle.integtests.fixtures.AbstractIntegrationSpec
 import org.gradle.integtests.fixtures.ToBeFixedForInstantExecution
 import spock.lang.Ignore
 import spock.lang.Issue
-import spock.lang.Unroll
-
 class FileCollectionIntegrationTest extends AbstractIntegrationSpec implements TasksWithInputsAndOutputs {
-    @Unroll
     def "can use 'as' operator with #type"() {
         buildFile << """
             def fileCollection = files("input.txt")
